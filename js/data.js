f76b7a3467864393de1b68028f4be96b4b4c77e8
chichone <cakyle@gmail.com>
2018-06-18 16:28:30 -0700

    Add hamburger menu button styling

--- js/data.js
+++ js/data.js
@@ -1,18 +1,22 @@
 var fav_strings = ['Curry Up Now, San Mateo', 'El Castillito, San Francisco', 'Souvla, San Francisco']
 
-var markers = [];
-
 var map;
 
 function initMap() {
+  console.log('docwidth', window.innerWidth);
+  var initial_zoom = 2;
+  if (window.innerWidth < 330 ){
+    initial_zoom = 13;
+  }
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.7413549, lng: -73.9980244},
-    zoom: 13
+    zoom: initial_zoom
   });
 
   console.log('map loaded');
   strings_to_markers();
+
 }
 
 //helper functions
