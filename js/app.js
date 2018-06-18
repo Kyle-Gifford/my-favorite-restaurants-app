3031a919c6b1d6afdcd003a9f872aa4516821a99
chichone <cakyle@gmail.com>
2018-06-17 20:43:45 -0700

    Properly load maps api using variable in google_api_key javascript file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-var my_key = google_api_key
-var favorites_strings = fav_strings
+//google_api_key
+//fav_strings
+maps_script = '<script async defer src="https://maps.googleapis.com/maps/api/js?key=' + google_api_key + '&v=3&callback=initMap"></script>'
 
+$("body").append(maps_script)
+
+var Model = function(){
+  this.locations = ko.observableArray();
+}
+
+var ViewModel = function(){
+
+  this.active_toggle = function(){
+    if (this.active == true) {
+      this.active = false
+    } else {
+      this.active = true
+    }
+  }
+
+}
+
+
+ko.applyBindings(new ViewModel());
