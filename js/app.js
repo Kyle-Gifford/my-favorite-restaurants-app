5b804023be10fd59db945034c4ded4b60af1f1d3
chichone <cakyle@gmail.com>
2018-06-19 15:58:51 -0700

    Model architecture created

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
-var Model = {
-  "restaurants": fav_strings
-};
 
-var yo = function(){
-  console.log('yo');
-}
+
+var Model = function(){
+  this.locs = [];
+  this.api_key = google_api_key;
+  this.fav_strings = fav_strings;
+  this.styles = styles;
+  this.getLocs = getLocs;
+  this.loadGoogle = loadGoogle;
+  this.googleLoadedCallback = googleLoadedCallback;
+  this.initMap = initMap;
+  this.init = function(){
+    self.loadGoogle();
+    cl('model init ran')
+  };
+
+
+  this.init();
+};
+model = new Model();
+
 
 var AppViewModel = function(){
+  this.init = function(){
+    console.log('AppViewModel init ran');
+  }
 
-  this.markers = ko.observableArray();
+  this.model = model;
 
   this.dropdownVisible = ko.observable((window.innerWidth > 330) ? 1 : 0);
 
@@ -17,11 +34,12 @@ var AppViewModel = function(){
   };
 
   this.handleMenuClick = function(self){
+    cl(self.model);
     self.dropdownVisible() ? self.dropdownVisible(0) : self.dropdownVisible(1);
   };
 
+  this.init();
 };
-
 
 
 ko.applyBindings(new AppViewModel());
--- js/data.js
+++ js/data.js
@@ -1,99 +1,132 @@
-var fav_strings = ['Curry Up Now, San Mateo', 'El Castillito, San Francisco', 'Souvla, San Francisco'];
+// TODO: remove cl and all logs
+var cl = function(i){
+  console.log(i);
+};
 
+
+var fav_strings = ['Curry Up Now, San Mateo', 'El Castillito, Church St, San Francisco', 'Souvla, Hayes, San Francisco'];
+
+var geocoder;
 var map;
 
-var loadMap = function(){
+var getLocs = function(){
+  geocoder = new google.maps.Geocoder();
+  var requests = [];
+  fav_strings.forEach(function(name){
+    requests.push({address: name})
+  });
+  var places = [];
+  function geocodes_complete_callback(){
+    cl('all geocodes complete');
+  }
+  var geocodes_completed = 0;
+  requests.forEach(function(request){
+    geocoder.geocode( { 'address': request.address}, function(results, status) {
+      model.locs.push(results[0]);
+      geocodes_completed ++;
+      if (geocodes_completed >= requests.length){geocodes_complete_callback();};
+  });
+  });
+};
+
+var loadGoogle = function(){
   var tag = document.createElement('script');
   tag.async = true;
   tag.defer = true;
-  tag.src = 'https://maps.googleapis.com/maps/api/js?key='+ google_api_key +'&v=3&callback=mapOnLoadCallback';
+  tag.src = 'https://maps.googleapis.com/maps/api/js?key='+ google_api_key +'&v=3&callback=googleLoadedCallback';
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-}();
-
-function mapOnLoadCallback(){
-  initMap();
-  yo();
 };
 
-function initMap() {
+
+function initMap(styles) {
   console.log('docwidth', window.innerWidth);
   var initial_zoom = 11;
   if (window.innerWidth < 330 ){
     initial_zoom = 10.5;
   }
   // Constructor creates a new map - only center and zoom are required.
-  var styles = [
-    {
-      featureType: 'water',
-      stylers: [
-        { color: '#19a0d8' }
-      ]
-    },{
-      featureType: 'administrative',
-      elementType: 'labels.text.stroke',
-      stylers: [
-        { color: '#ffffff' },
-        { weight: 6 }
-      ]
-    },{
-      featureType: 'administrative',
-      elementType: 'labels.text.fill',
-      stylers: [
-        { color: '#e85113' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'geometry.stroke',
-      stylers: [
-        { color: '#efe9e4' },
-        { lightness: -40 }
-      ]
-    },{
-      featureType: 'transit.station',
-      stylers: [
-        { weight: 9 },
-        { hue: '#e85113' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'labels.icon',
-      stylers: [
-        { visibility: 'off' }
-      ]
-    },{
-      featureType: 'water',
-      elementType: 'labels.text.stroke',
-      stylers: [
-        { lightness: 100 }
-      ]
-    },{
-      featureType: 'water',
-      elementType: 'labels.text.fill',
-      stylers: [
-        { lightness: -100 }
-      ]
-    },{
-      featureType: 'poi',
-      elementType: 'geometry',
-      stylers: [
-        { visibility: 'on' },
-        { color: '#f0e4d3' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'geometry.fill',
-      stylers: [
-        { color: '#efe9e4' },
-        { lightness: -25 }
-      ]
-    }
-  ];
-
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.685839, lng: -122.366792},
     zoom: initial_zoom,
     styles: styles,
-    mapTypeControl: false
+    mapTypeControl: false,
   });
-}
+};
+
+function googleLoadedCallback(){
+  initMap(styles);
+  getLocs();
+};
+
+
+
+
+
+
+var styles = [
+  {
+    featureType: 'water',
+    stylers: [
+      { color: '#19a0d8' }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { color: '#ffffff' },
+      { weight: 6 }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { color: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -40 }
+    ]
+  },{
+    featureType: 'transit.station',
+    stylers: [
+      { weight: 9 },
+      { hue: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'labels.icon',
+    stylers: [
+      { visibility: 'off' }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { lightness: 100 }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { lightness: -100 }
+    ]
+  },{
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      { visibility: 'on' },
+      { color: '#f0e4d3' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -25 }
+    ]
+  }
+];
